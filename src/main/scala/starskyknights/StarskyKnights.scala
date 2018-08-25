e7f6c07004b6b128631d4289c24e4f10c1f4f273
Warren Hamilton Ronsiek <warrenronsiek@gmail.com>
2018-08-25 14:10:14 -0700

    Finished part 3.

--- src/main/scala/starskyknights/StarskyKnights.scala
+++ src/main/scala/starskyknights/StarskyKnights.scala
@@ -1,16 +1,16 @@
 package starskyknights
 
-
+import scala.collection.mutable
 
 class StarskyKnights(nRows: Int, nCols: Int) {
 
-  def generateMoveSet(tile: (Int, Int)): Set[(Int, Int)] = {
+  def generateMoveSet(tile: (Int, Int), traversed: mutable.Map[(Int, Int), Boolean] = mutable.Map[(Int, Int), Boolean]().withDefaultValue(false)): Set[(Int, Int)] = {
     Set(
       (tile._1 - 1, tile._2 + 2), (tile._1 + 1, tile._2 + 2),
       (tile._1 + 2, tile._2 + 1), (tile._1 + 2, tile._2 - 1),
       (tile._1 + 1, tile._2 - 2), (tile._1 - 1, tile._2 - 2),
       (tile._1 - 2, tile._2 - 1), (tile._1 - 2, tile._2 + 1)
-    ).filter(t => t._1 >= 0 && t._2 >= 0 && t._1 < nCols && t._2 < nRows)
+    ).filter(t => t._1 >= 0 && t._2 >= 0 && t._1 < nCols && t._2 < nRows && !traversed(t))
   }
 
   def validateMoves(moves: List[(Int, Int)]): Boolean = {
@@ -25,5 +25,32 @@ class StarskyKnights(nRows: Int, nCols: Int) {
         }
       case Nil => true
     }
+  }
+
+  def computePath(start: (Int, Int), end: (Int, Int)): List[(Int, Int)] = {
+    var traversed = mutable.Map[(Int, Int), Boolean]().withDefaultValue(false)
+
+    def recursiveTraversal(path: List[(Int, Int)]): List[(Int, Int)] = {
+      val currentPosition = path.head
+      if (currentPosition == end) {
+        return path
+      }
+      traversed += (currentPosition -> true)
+      val possibleMoves = generateMoveSet(path.head, traversed)
+      if (possibleMoves == Set()) {
+        path
+      } else {
+        possibleMoves
+          .map(t => recursiveTraversal(t :: path))
+          .reduce[List[(Int, Int)]] {
+          case l if l._1.head == end && l._2.head == end => if (l._1.length > l._2.length) l._2 else l._1
+          case l if l._1.head == end && l._2.head != end => l._1
+          case l if l._2.head == end => l._2
+          case l => l._1
+        }
+      }
+    }
+
+    recursiveTraversal(List(start))
   }
 }
--- src/test/scala/StarskyKnightsTest.scala
+++ src/test/scala/StarskyKnightsTest.scala
@@ -1,18 +1,37 @@
-import org.scalatest.FunSuite
+import org.scalatest.{FunSpec, FunSuite}
 import starskyknights.StarskyKnights
 
-class StarskyKnightsTest extends FunSuite {
-  val sk = new StarskyKnights(4, 4)
+class StarskyKnightsTest extends FunSpec {
 
-  test("Generates Valid Moves") {
-    assert(sk.generateMoveSet((3,3)) === Set((1, 2), (2,1)))
+  describe("Basic Functionality") {
+    val sk = new StarskyKnights(4, 4)
+    it("Generates Valid Moves") {
+      assert(sk.generateMoveSet((3,3)) === Set((1, 2), (2,1)))
+    }
+
+    it("Validates Valid Path") {
+      assert(sk.validateMoves(List((3,2), (2, 0), (1, 2), (3,3))))
+    }
+
+    it( "Invalidates Invalid Path") {
+      assert(sk.validateMoves(List((3, 2), (3, 0), (1, 1), (3, 1))) === false)
+    }
   }
 
-  test("Validates Valid Path") {
-    assert(sk.validateMoves(List((3,2), (2, 0), (1, 2), (3,3))))
+  describe("Path Finding Functionality") {
+    val sk4 = new StarskyKnights(4, 4)
+    val sk12 = new StarskyKnights(12, 12)
+
+    it("Solves Trivial Path") {
+      val path = sk4.computePath((0, 0), (2, 1))
+      assert(sk4.validateMoves(path))
+    }
+
+    it("Solves Slightly Less Trivial Path") {
+      val path = sk12.computePath((1,7), (2, 10))
+      assert(sk12.validateMoves(path))
+    }
+
   }
 
-  test( "Invalidates Invalid Path") {
-    assert(sk.validateMoves(List((3, 2), (3, 0), (1, 1), (3, 1))) === false)
-  }
 }
